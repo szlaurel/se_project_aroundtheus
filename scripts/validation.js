6e1bc70f2345dfece97f1b8d883e5c5f4b914dbc
szlaurel <szlaurel5@gmail.com>
2023-04-25 21:24:52 -0700

    the calm code before the storm

---
+++ scripts/validation.js
@@ -0,0 +1,11 @@
+// enabling validation by calling enableValidation()
+// pass all the settings on call
+
+enableValidation({
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+});
