9b85631ebf0de5172b778e2b52532c39e1365f42
szlaurel <szlaurel5@gmail.com>
2023-04-26 00:27:33 -0700

    everything looks good so far, just changed inputErrorClass with errorClass in show and hide input error functions

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,6 +1,50 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+const showInputError = (input, formEl, { errorClass }) => {
+  const errorSpan = formEl.querySelector(`#${input.id}-error`);
+  // add error messsage/class
+  console.log(input.validationMessage);
+  errorSpan.textContent = input.validationMessage;
+  input.classList.add(errorClass);
+};
+
+const hideInputError = (input, formEl, { errorClass }) => {
+  const errorSpan = formEl.querySelector(`#${input.id}-error`);
+  // add error messsage/class
+  errorSpan.textContent = " ";
+  input.classList.remove(errorClass);
+};
+
+const checkInputValidity = (formEl, input, options) => {
+  if (input.validity.valid) {
+    hideInputError(input, formEl, options);
+  } else {
+    showInputError(input, formEl, options);
+  }
+};
+
+const setEventListeners = (formEl, options) => {
+  const inputs = [...formEl.querySelectorAll(options.inputSelector)];
+  inputs.forEach((input) => {
+    input.addEventListener("input", (evt) => {
+      // check validity
+      checkInputValidity(formEl, input, options);
+      // add error message/class
+
+      // toggle the button
+    });
+  });
+};
+
+const enableValidation = (options) => {
+  const formElements = [...document.querySelectorAll(options.formSelector)];
+  formElements.forEach((formEl) => {
+    formEl.addEventListener("submit", (evt) => evt.preventDefault());
+    setEventListeners(formEl, options);
+  });
+};
+
 enableValidation({
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
@@ -9,3 +53,10 @@ enableValidation({
   inputErrorClass: "modal__input_type_error",
   errorClass: "modal__error_visible",
 });
+
+// forms
+// inputs forms
+// input ===
+
+// tasks
+// all thats left to do is to code the button toggle state
