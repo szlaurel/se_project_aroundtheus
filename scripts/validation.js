0be9e1ad03a8d24f08fd19d0dc18a50b99ede1ee
szlaurel <szlaurel5@gmail.com>
2023-04-26 18:07:49 -0700

    make changes to validation.js to work

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,6 +1,8 @@
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+// shows the input error if the input doesn't pass validation
+
 const showInputError = (input, formEl, { errorClass }) => {
   const errorSpan = formEl.querySelector(`#${input.id}-error`);
   // add error messsage/class
@@ -9,6 +11,8 @@ const showInputError = (input, formEl, { errorClass }) => {
   input.classList.add(errorClass);
 };
 
+//hides the input error
+
 const hideInputError = (input, formEl, { errorClass }) => {
   const errorSpan = formEl.querySelector(`#${input.id}-error`);
   // add error messsage/class
@@ -16,6 +20,7 @@ const hideInputError = (input, formEl, { errorClass }) => {
   input.classList.remove(errorClass);
 };
 
+//checks to see if the inputs are valid and if their not they'll show error signs
 const checkInputValidity = (formEl, input, options) => {
   if (input.validity.valid) {
     hideInputError(input, formEl, options);
@@ -24,15 +29,42 @@ const checkInputValidity = (formEl, input, options) => {
   }
 };
 
+// looks to see if all the inputs are valid
+const hasValidInputs = (inputList) => {
+  return inputList.every((input) => input.validity.valid === true);
+};
+
+function disableButton(submitButton, options) {
+  submitButton.disabled = true;
+  submitButton.classList.add(options.inactiveButtonClass);
+}
+function enableButton(submitButton, options) {
+  submitButton.disabled = false;
+  submitButton.classList.remove(options.inactiveButtonClass);
+}
+
+const toggleButtonState = (inputList, submitButton, options) => {
+  console.log("hasValid", hasValidInputs(inputList));
+  // if all inputs are valid
+  if (!hasValidInputs(inputList)) {
+    disableButton(submitButton, options);
+    // make button disabled
+  } else {
+    enableButton(submitButton, options);
+    // make button enabled
+  }
+};
+
 const setEventListeners = (formEl, options) => {
-  const inputs = [...formEl.querySelectorAll(options.inputSelector)];
-  inputs.forEach((input) => {
+  const inputList = [...formEl.querySelectorAll(options.inputSelector)];
+  const submitButton = formEl.querySelector(options.submitButtonSelector);
+  inputList.forEach((input) => {
     input.addEventListener("input", (evt) => {
       // check validity
       checkInputValidity(formEl, input, options);
       // add error message/class
-
       // toggle the button
+      toggleButtonState(inputList, submitButton, options);
     });
   });
 };
@@ -60,3 +92,5 @@ enableValidation({
 
 // tasks
 // all thats left to do is to code the button toggle state
+// next you need to fix the input within the parameters and change it to "inputEl"
+// after that you need to code the styles properly when the errors come in the spacing underneath the title isnt spacing so far apart
